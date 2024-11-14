84fe0dffc343c433c2bb0293bc119d6741a6898e
Reginalealcouto <reginalealcouto@gmail.com>
2024-11-14 11:39:17 -0300

    Update aula09_mapa.py

--- aula09_mapa.py
+++ aula09_mapa.py
@@ -18,7 +18,7 @@ if st.checkbox('Mostrar tabela'):
 st.map(dadosFiltrados, latitude="Lat_d", longitude="Long_d")
 
 qtdeMunicipios = len(df['NM_MUNIC'].unique())
-st.write("A quantidade de municípios com localização quilombola é " + str(qtdeMunicipios))
+st.write("A quantidade de municípios com localização quilombola é {str(qtdeMunicipios)}.")
 
 qtdeComunidades = len(df['NM_AGLOM'].unique())
 st.write("A quantidade de comunidades quilombolas é " + str(qtdeComunidades))
