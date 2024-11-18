e8248b94715f16d2ebb9685cab4c9dc86313736b
Reginalealcouto <reginalealcouto@gmail.com>
2024-11-18 09:54:42 -0300

    Create aula_10.py

---
+++ aula_10.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+url ='https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=F&ordem=ASC&ordenarPor=nome'
+
+resposta = requests.get(url)
+
+dfmulheres = pd.DataFrame(resposta.json()['dados'])
+
+url ='https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=M&ordem=ASC&ordenarPor=nome'
+
+respostaM = requests.get(url)
+
+dfhomens = pd.DataFrame(respostaM.json()['dados'])
+
+df = pd.concat([dfmulheres, dfhomens])
+
+st.write(df)
+opcao = st.selectbox(
+    'Qual gênero vc quer selecionar?',
+     ['Feminino','Masculino'])
+
+st.write('Você selecionou: ', opcao)
+
+if opcao == 'Feminino':
+  st.write(dfmulheres)
+else:
+  st.write(dfhomens)
